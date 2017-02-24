0d302f2ba99288e99180a33919c7fd6be46b11a0
Anna <anyavalueva@gmail.com>
2017-02-24 13:38:25 -0800

    adde js - not working yet

---
+++ js/script.js
@@ -0,0 +1,11 @@
+
+
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "navigation") {
+        x.className += " responsive";
+    } else {
+        x.className = "navigation";
+    }
+}
+
