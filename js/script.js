10e0e85afde6ce5a120e0f350469df79fd01a3d6
Anna <anyavalueva@gmail.com>
2017-03-03 18:29:24 -0800

    created checkout page + CONTENT

--- js/script.js
+++ js/script.js
@@ -1,11 +1,18 @@
+var acc = document.getElementsByClassName("accordion");
+var i;
 
+for (i = 0; i < acc.length; i++) {
+    acc[i].onclick = function(){
+        /* Toggle between adding and removing the "active" class,
+        to highlight the button that controls the panel */
+        this.classList.toggle("active");
 
-function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "navigation") {
-        x.className += " responsive";
-    } else {
-        x.className = "navigation";
+        /* Toggle between hiding and showing the active panel */
+        var panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+            panel.style.display = "none";
+        } else {
+            panel.style.display = "block";
+        }
     }
 }
-
